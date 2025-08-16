e0e8b5d4fdbf754affc3ceafe7078de795f9669d
Andrew Chalmaev <andr.ch@gmail.com>
2025-08-16 12:00:14 +0300

    install added

---
+++ install.ps1
@@ -0,0 +1,3 @@
+#!/bin/pwsh
+
+copy Microsoft.PowerShell_profile.ps1 $Env:Userprofile\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/ 
