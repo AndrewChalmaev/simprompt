3d2b55423479a0e19d80091659bf9acf2ae27415
Andrew Chalmaev <andr.ch@gmail.com>
2025-08-16 12:09:24 +0300

    install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
 #!/bin/pwsh
 
-copy Microsoft.PowerShell_profile.ps1 $Env:Userprofile\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+copy Microsoft.PowerShell_profile.ps1 $Home\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
