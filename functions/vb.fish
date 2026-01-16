bed6995be32c2bcc9784985c808cfe971413a9ce
AndrewChalmaev <andr.ch@gmail.com>
2026-01-16 23:49:53 +0300

    kstart in vb function

--- functions/vb.fish
+++ functions/vb.fish
@@ -2,5 +2,5 @@ function vb --description 'VirtualBox start script'
   sudo modprobe -r kvm_intel
   sudo chown andrew:andrew /dev/sd*
   ll /dev/sd*
-  kstart VirtualBox $argv
+  kstart VirtualBox $argv 2>/dev/null
 end
