1b99e4a03c85cda2eefceb877dd115ff647549b7
AndrewChalmaev <andr.ch@gmail.com>
2026-03-11 12:49:08 -0700

    jmk.ps1

--- jmk.ps1
+++ jmk.ps1
@@ -1,44 +1,43 @@
 #!/bin/env pwsh
 
-SOURCE=C:/Users/andrew
-# SRC_AV=mnt/t
-short=(
-  aud
-  desk
-  doc
-  down
-#  goo
-#  ins
-#  one
-#  docs
-#  pics
-  pic
-#  scr
-  vid
-#  yad
+$SOURCE="Users/andrew"
+# $SRC_AV="mnt/t"
+
+$short=@(
+  "aud",
+  "desk",
+  "doc",
+  "down",
+#  "goo",
+#  "ins",
+#  "one",
+#  "docs",
+#  "pics",
+  "pic",
+#  "scr",
+  "vid"
+# " yad"
 )
-long=(
-  $SOURCE/Music
-#  $SRC_AV/Music
-  $SOURCE/Desktop
-  $SOURCE/Documents
-  $SOURCE/Downloads
-#  $SOURCE/GoogleDrive
-#  $SRC_AV/install
-#  $SOURCE/OneDrive
-#  $SOURCE/OneDrive/docs
-#  $SOURCE/OneDrive/pics
-#  $SOURCE/Pictures
-#  $SOURCE/Pictures/Screenshots
-  $SOURCE/Videos
-#  $SRC_AV/Videos
-#  $SOURCE/Yandex.Disk
+$long=@(
+  "$SOURCE/Music",
+# " $SRC_AV/Music",
+  "$SOURCE/Desktop",
+  "$SOURCE/Documents",
+  "$SOURCE/Downloads",
+#  "$SOURCE/GoogleDrive",
+#  "$SRC_AV/install",
+#  "$SOURCE/OneDrive",
+#  "$SOURCE/OneDrive/docs",
+#  "$SOURCE/OneDrive/pics",
+  "$SOURCE/Pictures",
+#  "$SOURCE/Pictures/Screenshots",
+  "$SOURCE/Videos"
+#  "$SRC_AV/Videos",
+#  "$SOURCE/Yandex.Disk"
 )
 
-# sudo New-Item -Path "C:\desk\" -ItemType SymbolicLink -Target "C:\Users\andrew\Desktop\"
 cd /
-for i in ${!short[*]}; do
-  printf "%s: %s\n" ${short[$i]} ${long[$i]}
-  rm -f ${short[$i]}
-  ln -s ${long[$i]} ${short[$i]}
-done
+for ($i=0; $i -lt $short.Count; $i++) {
+  if (Test-Path $short[$i]) {Remove-Item -Path $short[$i]}
+  sudo New-Item -Path $short[$i] -ItemType SymbolicLink -Target $long[$i]
+}
